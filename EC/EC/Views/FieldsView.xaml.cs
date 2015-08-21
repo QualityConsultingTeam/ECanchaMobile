ee6110e27813e74cebb39b7566e225f60a8f8e60
Jorge Flores <jorgeafc_00h@hotmail.com>
2015-08-21 01:37:47 -0600

    First Commit

---
+++ EC/EC/Views/FieldsView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EC.Views
+{
+    public partial class FieldsView : ContentPage
+    {
+        public FieldsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
