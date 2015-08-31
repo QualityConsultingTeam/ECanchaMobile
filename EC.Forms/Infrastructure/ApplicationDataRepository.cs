4b9acf586497e6f80b0390ebdd12e768bac8c080
Jorge Flores <jorgeafc_00h@hotmail.com>
2015-08-31 09:08:04 -0600

    Fields Viewmodel

---
+++ EC.Forms/Infrastructure/ApplicationDataRepository.cs
@@ -0,0 +1,50 @@
+﻿using EC.Client.Core.Infrastructure.Abstractions.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC.Forms.Infrastructure
+{
+    public class ApplicationDataRepository : IApplicationDataRepository
+    {
+        private const string StorageGroupKey = "Default";
+
+        // private static readonly SimpleStorage StorageGroup = SimpleStorage.EditGroup(StorageGroupKey);
+
+        public string GetStringFromApplicationData(string key, string defaultValue)
+        {
+            //var returnString = StorageGroup.HasKey(key) ?
+            //    StorageGroup.Get<string>(key) :
+            //    defaultValue;
+            //return returnString;
+            return defaultValue;
+        }
+
+        public bool? GetOptionalBooleanFromApplicationData(string key, bool? defaultValue)
+        {
+            //var returnBool = StorageGroup.HasKey(key) ? 
+            //    StorageGroup.Get<bool?>(key) : 
+            //    defaultValue;
+
+            //return returnBool;
+            return defaultValue;
+        }
+
+        public int? GetOptionalIntegerFromApplicationData(string key, int? defaultValue)
+        {
+            //var returnInt = StorageGroup.HasKey(key) ? 
+            //    StorageGroup.Get<int?>(key) : 
+            //    defaultValue;
+
+            //return returnInt;
+            return defaultValue;            
+        }
+
+        public void SetStringToApplicationData(string key, string value)
+        {
+            // StorageGroup.Put<string>(key, value);
+        }
+    }
+}
---
+++ EC.Forms/Views/FieldView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using EC.Client.Core.Infrastructure;
+using EC.Client.Core.ServiceAgents;
+using EC.Forms.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EC.Forms.Views
+{
+    public partial class FieldView : ContentPage
+    {
+        public FieldView()
+        {
+            InitializeComponent();
+            // this.BindingContext = new FieldsViewModel(this);
+
+            this.BindingContext = new FieldsViewModel(this);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ((FieldsViewModel)this.BindingContext).LoadData();
+        }
+
+    }
+}
