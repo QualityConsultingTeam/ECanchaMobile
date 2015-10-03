6e5cd6341e1d6201b26e2756455605424c7ffa5f
gamez93 <nls.gmz.93@gmail.com>
2015-10-03 12:21:55 -0600

    cambios

--- EC/EC/ServiceAgents/Interfaces/IAccountService.cs
+++ EC/EC/ServiceAgents/Interfaces/IAccountService.cs
@@ -8,6 +8,8 @@ namespace EC.ServiceAgents.Interfaces
 {
     public interface IAccountService : IUpdatableUrl
     {
+        Task<bool> Register(string Email, string Password, bool RefreshToken);
 
+        Task<bool> Login(string Email, string Password, bool RefreshToken);
     }
 }
