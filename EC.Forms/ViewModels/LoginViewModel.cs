cb427be5d7b15ac4dec755d12d4e28234c1788d3
gamez93 <nls.gmz.93@gmail.com>
2015-10-03 11:58:57 -0600

    primeros cambios

---
+++ EC.Forms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC.Forms.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        #region Public Properties
+
+        public string Email {
+            get { return _email; }
+            set { SetProperty(ref _email, value); }
+        }
+
+        public string Password {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        #endregion
+
+        #region Private Properties
+
+        private string _email; //{ get; set; }
+        private string _password; //{ get; set; }
+
+        #endregion
+
+
+
+    }
+}//cierre de clase
